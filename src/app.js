fb94910438f20f48d7fd4befc751133682403bf2
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-04-23 09:36:35 +0430

    added Start action to app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
+const path = require("path");
 const { Telegraf } = require("telegraf");
 const bot = new Telegraf(process.env.ROBOT_TOKEN);
-
+const Start = require(path.join(__dirname, "./Actions/Start/Start"));
+bot.start(Start);
 module.exports = bot;
