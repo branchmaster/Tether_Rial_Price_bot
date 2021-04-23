706f9a105c55610b3683bc83afb178f2d78f2f9b
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-04-23 09:51:27 +0430

    added About Hears to app

--- src/app.js
+++ src/app.js
@@ -2,5 +2,7 @@ const path = require("path");
 const { Telegraf } = require("telegraf");
 const bot = new Telegraf(process.env.ROBOT_TOKEN);
 const Start = require(path.join(__dirname, "./Actions/Start/Start"));
+const About = require(path.join(__dirname, "./Hears/About/About"));
 bot.start(Start);
+bot.hears("درباره ما 👤", About);
 module.exports = bot;
