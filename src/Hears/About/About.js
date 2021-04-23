f27328751d46c293a65b221b9b2ae059ad639aab
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-04-23 09:51:15 +0430

    added About Hears

---
+++ src/Hears/About/About.js
@@ -0,0 +1,7 @@
+module.exports = async (ctx) => {
+  return ctx.reply(`
+       آیدی سازنده : @Mohammadizanloo55
+       لینک گیت هاب جهت استار و کانتربیوت 😁  :
+       https://github.com/mohammadizanloo55/Tether_Rial_Price_bot
+    `);
+};
