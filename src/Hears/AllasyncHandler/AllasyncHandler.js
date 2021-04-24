a15c0aed77cb6204a087ff335a43859d25673ff4
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-04-24 07:22:03 +0430

    added Price in letters

--- src/Hears/AllasyncHandler/AllasyncHandler.js
+++ src/Hears/AllasyncHandler/AllasyncHandler.js
@@ -4,18 +4,20 @@ const SplitedPrice = require(path.join(
   __dirname,
   "./../../Helper/SplitedPrice/SplitedPrice"
 ));
+const { NumberToWords } = require("@persian-tools/persian-tools");
+
 module.exports = async (ctx, websitename, websitelink) => {
   return axios
     .get(websitename)
     .then((value) => {
       const convertedprice = SplitedPrice(parseInt(value.data.price));
+      const convertedpricetoword = `${NumberToWords.convert(
+        value.data.price
+      )} تومان`;
       const Time = value.data.date;
-      return ctx.reply(`
-        قیمت تتر به تومان💰: ${convertedprice}
-        زمان به ساعت جهانی 🌎⏳ :  ${Time}
-        لینک سایت 📈 : ${websitelink}
-                
-      `);
+      return ctx.reply(
+        `قیمت تتر به تومان (عددی)💰: ${convertedprice}\nقیمت تتر به تومان (حروفی) 💰 : ${convertedpricetoword}\nزمان به ساعت جهانی 🌎⏳ :  ${Time}\nلینک سایت 📈 : ${websitelink}`
+      );
     })
     .catch((err) => {
       return console.log(err);
